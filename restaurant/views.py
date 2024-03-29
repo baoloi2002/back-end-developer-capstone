e6b931637294f06d2fe3daf08d0481b77400d883
Tran Bao Loi <59519701+baoloi2002@users.noreply.github.com>
2024-03-29 14:35:48 +0700

    feat: api booking table

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
 
 
 def index(request):
@@ -19,3 +20,8 @@ class SingleMenuItemView(
 ):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
