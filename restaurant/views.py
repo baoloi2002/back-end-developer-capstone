482923cdb63eb8de37348d37a360281132911c13
Tran Bao Loi <59519701+baoloi2002@users.noreply.github.com>
2024-03-29 14:23:02 +0700

    feat: DRF API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
 
 
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(
+    generics.RetrieveUpdateDestroyAPIView, generics.DestroyAPIView
+):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
